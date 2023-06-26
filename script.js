4a83b2b94cf28e3bf092f98b51af3367a5c13fb6
Micky22 <119489685+Mikcy22@users.noreply.github.com>
2023-06-26 18:23:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function buscarPelicula() {
+    var titulo = document.getElementById("titulo-input").value;
+    var url = "http://www.omdbapi.com/?apikey=d4222942&t=" + encodeURIComponent(titulo);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        mostrarResultado(data);
+      }
+    };
+    xhr.send();
+  }
+  
+  function mostrarResultado(data) {
+    var resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = "";
+  
+    if (data.Response === "True") {
+      var titulo = data.Title;
+      var anio = data.Year;
+      var director = data.Director;
+      var poster = data.Poster;
+      var descripcion = data.Plot;
+  
+      var imagen = document.createElement("img");
+      imagen.src = poster;
+      resultadoDiv.appendChild(imagen);
+  
+      var tituloP = document.createElement("p");
+      tituloP.innerHTML = "<strong>Título:</strong> " + titulo;
+      resultadoDiv.appendChild(tituloP);
+  
+      var anioP = document.createElement("p");
+      anioP.innerHTML = "<strong>Año:</strong> " + anio;
+      resultadoDiv.appendChild(anioP);
+  
+      var directorP = document.createElement("p");
+      directorP.innerHTML = "<strong>Director:</strong> " + director;
+      resultadoDiv.appendChild(directorP);
+  
+      var descripcionP = document.createElement("p");
+      descripcionP.innerHTML = "<strong>Descripción:</strong> " + descripcion;
+      resultadoDiv.appendChild(descripcionP);
+    } else {
+      resultadoDiv.innerHTML = "No se encontraron resultados.";
+    }
+  }
+  
+  function buscarConEnter(event) {
+    if (event.keyCode === 13) {
+      // El código 13 corresponde a la tecla Enter
+      buscarPelicula();
+      event.preventDefault(); // Evitar el comportamiento por defecto del Enter (por ejemplo, envío de formularios)
+    }
+  }
+  
